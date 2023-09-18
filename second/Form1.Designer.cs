4bdb23b6a20d65c2434974fea239a79da1f72211
Ruslana Gilmetdinova <gilmetdinovaruslana@gamil.com>
2023-09-18 21:10:51 +0500

    Форма длинная и оранжевая

--- second/Form1.Designer.cs
+++ second/Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace second
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(24, 205);
+            this.button1.Location = new System.Drawing.Point(200, 74);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(119, 51);
             this.button1.TabIndex = 0;
@@ -46,8 +46,8 @@ namespace second
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Khaki;
-            this.ClientSize = new System.Drawing.Size(275, 277);
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(619, 185);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
